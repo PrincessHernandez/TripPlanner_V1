11e7e4ac4aea7d81fd91aa320d8579d6b338a9ce
Bojan-sensei <31077566+Bojan-sensei@users.noreply.github.com>
2018-01-14 15:37:20 -0500

    History

--- app/src/main/java/travelers/tripplanner/fragments/history.java
+++ app/src/main/java/travelers/tripplanner/fragments/history.java
@@ -26,13 +26,91 @@ import travelers.tripplanner.R;
 
 public class history extends Fragment {
 
+    private ArrayList<String> place_name, place_address;
+    private ListView list;
+    protected HistoryAdapter adapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_history, container, false);
 
+        place_name = new ArrayList<>();
+        place_address = new ArrayList<>();
+        //list.setAdapter(adapter);
+
+        DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference mUserIdRef = mRootRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        DatabaseReference mBucketList = mUserIdRef.child("BucketList");
+        mBucketList.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                place_name = new ArrayList<>();
+                place_address = new ArrayList<>();
+                for (DataSnapshot tripSnap: dataSnapshot.getChildren()) {
+                    for(DataSnapshot placeSnap: tripSnap.getChildren()){
+                        String tempName = new String();
+                        String tempAddress = new String();
+                        for(DataSnapshot placeAttributeSnap: placeSnap.getChildren()){
+                            if(placeAttributeSnap.getKey().equals("visited")) {
+                                if (placeAttributeSnap.getValue(Boolean.class)) {
+                                    place_name.add(tempName);
+                                    place_address.add(tempAddress);
+                                }
+                            }
+                            if(placeAttributeSnap.getKey().equals("name")) {
+                                tempName = placeAttributeSnap.getValue(String.class);
+                            }
+                            if(placeAttributeSnap.getKey().equals("address")) {
+                                tempAddress = placeAttributeSnap.getValue(String.class);
+                            }
+                        }
+                    }
+                }
+                adapter = new HistoryAdapter(getActivity(), place_name, place_address);
+                list = view.findViewById(R.id.history_lv);
+                list.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         return view;
+    }
+
+    private class HistoryAdapter extends ArrayAdapter<String> {
+        private ArrayList<String> name, address;
+        private Activity mActivity;
+
+        public HistoryAdapter(@NonNull Activity activity, ArrayList<String> name, ArrayList<String> address) {
+            super(activity, R.layout.history_item, name);
+            this.name = name;
+            this.address = address;
+            this.mActivity = activity;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            View mView = convertView;
+            if(mView==null){
+                LayoutInflater inflater = mActivity.getLayoutInflater();
+                mView = inflater.inflate(R.layout.history_item, null, true);
+            }
+
+            TextView place_name = mView.findViewById(R.id.place_name_checked_tv);
+            TextView place_address = mView.findViewById(R.id.place_address_checked_tv);
+
+            place_name.setText(String.format("Place: %s", this.name.get(position)));
+            place_address.setText(String.format("Address: %s", this.address.get(position)));
+
+            return mView;
+        }
     }
 }
